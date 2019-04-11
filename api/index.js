07f41c66a65e7d23385d719b8765527ee3724351
Pablo Varela <pablovarela182@gmail.com>
2019-04-11 15:53:59 +0200

    Remove scrapper, reverse engineering API and deploy as lambda

--- api/index.js
+++ api/index.js
@@ -1,38 +1,90 @@
-const { parse } = require('url')
-const micro = require('micro')
-const ms = require('ms')
-const scrapper = require('./scrapper')
+const got = require('got');
+const cheerio = require('cheerio');
 
-let _cache = null
+// 1 => 01
+// 11 => 11
+const padDate = n => {
+  if (n.toString().length < 2) {
+    return '0' + n;
+  }
 
-const populateCache = async () => {
-  const rp = await scrapper('raxo', 'pontevedra')
-  const pr = await scrapper('pontevedra', 'raxo')
+  return n;
+};
 
-  console.log(rp)
-
-  _cache = { rp, pr }
-}
-
-const setHeaders = (res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET')
-
-  return res
-}
-
-const server = micro(
-  async (req, res) => {
-    res = setHeaders(res)
-
-    const { query } = parse(req.url, true)
-
-    if (query.refreshCache != null || _cache === null) {
-      await populateCache()
+const buildUrl = (from, to) => {
+  const baseUrl = 'http://www.monbus.es/';
+  const now = new Date();
+  const params = {
+    route: '/src/net/monbus/horarios/trigger/results.php',
+    'data[captcha]': 0,
+    'data[searchType]': 1,
+    'data[paradaOrigen]': from,
+    'data[paradaDestino]': to,
+    'data[nViajeros]': 1,
+    'data[tipoBillete]': 1,
+    'data[fechaIda]': `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`,
+  };
+  const paramsString = Object.keys(params).reduce((acc, curr) => {
+    const str = `${curr}=${params[curr]}`;
+    if (acc === '') {
+      return str;
     }
 
-    return _cache
-  }
-)
+    return `${acc}&${str}`;
+  }, '');
 
-server.listen(3000)
+  return `${baseUrl}?${paramsString}`;
+};
+
+const headers = {
+  accept: '*/*',
+  'accept-language': 'es-ES,es;q=0.9,en;q=0.8,gl;q=0.7',
+  'x-requested-with': 'XMLHttpRequest',
+};
+
+async function idas(from, to) {
+  const {body} = await got(buildUrl(from, to), {
+    credentials: 'include',
+    headers,
+    referrer: 'http://www.monbus.es/es',
+    referrerPolicy: 'no-referrer-when-downgrade',
+    body: null,
+    method: 'GET',
+    mode: 'cors',
+  });
+
+  const $ = cheerio.load(body);
+
+  let idas = [];
+  $('.departureTime').each((i, el) => idas.push($(el).text()));
+  idas = idas.reduce((acc, curr) => {
+    if (acc.includes(curr)) {
+      const index = acc.indexOf(curr);
+      acc[index] = acc[index] + ' (x2)';
+      return acc;
+    }
+    acc.push(curr);
+    return acc;
+  }, []);
+
+  return idas;
+}
+
+const PONTEVEDRA = 10530
+const RAXO = 10556
+
+module.exports = async (req, res) => {
+  try {
+    const pr = await idas(PONTEVEDRA, RAXO)
+    const rp = await idas(RAXO, PONTEVEDRA)
+
+    const responseObject = { pr, rp }
+
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(responseObject))
+  } catch (e) {
+    res.statusCode = 500
+    res.end('error')
+    console.log(e.message)
+  }
+}
